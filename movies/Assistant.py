7410296d6bdd5bebc5b8ca3e18ea5d0c4e7a91a4
Belal <bnwiran@gmail.com>
2025-03-13 07:24:40 -0400

    Refactor the AI assistant to a separate module

---
+++ movies/Assistant.py
@@ -0,0 +1,102 @@
+from typing import TypedDict, Annotated, Literal
+
+from langchain_core.messages import AIMessage
+from langgraph.constants import END, START
+from langgraph.graph import add_messages, StateGraph
+from langgraph.prebuilt import ToolNode
+
+# This is the message with which the system opens the conversation.
+WELCOME_MSG = "Welcome to the MovieRecBot chat. Type `q` to quit. How may I serve you today?"
+
+MOVIEBOT_SYSINT = (
+    "system",  # 'system' indicates the message is a system instruction.
+    "You are a MovieRecBot, an interactive movie recommender system. A human will talk to you about the "
+    "available movies you have and you will answer any questions about movie items in the list (and only about "
+    "movie items - no off-topic discussion, but you can chat about the movie, their director, the actors, and their history). "
+    "The customer will place an order for 1 or more items from the movie list, which you will structure "
+    "and send to the ordering system after confirming the order with the human. "
+    "\n\n"
+    "To show the MOVIE_LIST to the user, you should call the get_movies tool. The movie list is in JSON format."
+    "If you need to obtain the details about a specific movie, call the get_movie_details tool with the movie ID."
+    "If you know additional details about the movie, even if not listed, you can tell the user, clearly stating that the information is not in the list."
+    "Once the customer has chosen a movie, thank them and say you hope they enjoy the movie. ",
+)
+
+
+
+class AgentState(TypedDict):
+    """State representing the customer's order conversation."""
+
+    # The chat conversation. This preserves the conversation history
+    # between nodes. The `add_messages` annotation indicates to LangGraph
+    # that state is updated by appending returned messages, not replacing
+    # them.
+    messages: Annotated[list, add_messages]
+
+    # The customer's in-progress order.
+    order: list[str]
+
+    # Flag indicating that the order is placed and completed.
+    finished: bool
+
+
+
+class Agent:
+    def __init__(self, llm, tools: list):
+        self.llm = llm
+        self.graph = self._build_graph(tools)
+
+    def invoke(self, messages):
+        return self.graph.invoke(messages)
+
+
+    def _build_graph(self, tools: list):
+        tool_node = ToolNode(tools)
+        self.llm = self.llm.bind_tools(tools)
+
+        # Set up the initial graph based on our state definition.
+        graph_builder = StateGraph(AgentState)
+
+        # Add the chatbot function to the app graph as a node called "chatbot" and the automated tools calling
+        graph_builder.add_node("chatbot", self._chatbot)
+        graph_builder.add_node("tools", tool_node)
+
+        # Decide if tools need to be called
+        graph_builder.add_conditional_edges("chatbot", self._maybe_route_to_tools, {"tools": "tools", END: END})
+
+        # Tools always route back to chat afterward.
+        graph_builder.add_edge("tools", "chatbot")
+        # Define the chatbot node as the app entrypoint.
+        graph_builder.add_edge(START, "chatbot")
+        chat_graph = graph_builder.compile()
+
+        return chat_graph
+
+    def _chatbot(self, state: AgentState) -> AgentState:
+        """The chatbot itself. A wrapper around the model's own chat interface."""
+        print("IN CHATBOT----------------")
+        print("State:", state)
+        defaults = {"order": [], "finished": False}
+
+        if state["messages"] and len(state["messages"]) > 0:
+            new_output = self.llm.invoke([MOVIEBOT_SYSINT] + state["messages"])
+        else:
+            # If there are no messages, start with the welcome message.
+            new_output = AIMessage(content=WELCOME_MSG)
+
+        return defaults | state | {"messages": [new_output]}
+
+    def _maybe_route_to_tools(self, state: AgentState) -> Literal["tools", "chatbot", "__end__"]:
+        """Routes to the tools node if the last message was a tool call."""
+        msgs = state["messages"]
+
+        # Only route based on the last message.
+        msg = msgs[-1]
+        print("ROUTING: ", msg)
+        # When the chatbot returns tool_calls, route to the "tools" node.
+        if hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
+            print("going to tools")
+            return "tools"
+        else:
+            print("going to END")
+            return END
--- movies/views/chat.py
+++ movies/views/chat.py
@@ -1,56 +1,16 @@
-import os
 from pathlib import Path
-from typing import Annotated
-from typing import Literal
 
 import requests
 from django.shortcuts import render
 from dotenv import load_dotenv
-from langchain_core.messages.ai import AIMessage
 from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode
-from typing_extensions import TypedDict
 
+from movies.Assistant import Agent
 from movies.forms import ChatForm
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 _ = load_dotenv(BASE_DIR / '.env')
-
-
-class AgentState(TypedDict):
-    """State representing the customer's order conversation."""
-
-    # The chat conversation. This preserves the conversation history
-    # between nodes. The `add_messages` annotation indicates to LangGraph
-    # that state is updated by appending returned messages, not replacing
-    # them.
-    messages: Annotated[list, add_messages]
-
-    # The customer's in-progress order.
-    order: list[str]
-
-    # Flag indicating that the order is placed and completed.
-    finished: bool
-
-MOVIEBOT_SYSINT = (
-    "system",  # 'system' indicates the message is a system instruction.
-    "You are a MovieRecBot, an interactive movie recommender system. A human will talk to you about the "
-    "available movies you have and you will answer any questions about movie items in the list (and only about "
-    "movie items - no off-topic discussion, but you can chat about the movie, their director, the actors, and their history). "
-    "The customer will place an order for 1 or more items from the movie list, which you will structure "
-    "and send to the ordering system after confirming the order with the human. "
-    "\n\n"
-    "To show the MOVIE_LIST to the user, you should call the get_movies tool. The movie list is in JSON format."
-    "If you need to obtain the details about a specific movie, call the get_movie_details tool with the movie ID."
-    "If you know additional details about the movie, even if not listed, you can tell the user, clearly stating that the information is not in the list."
-    "Once the customer has chosen a movie, thank them and say you hope they enjoy the movie. ",
-)
-
-# This is the message with which the system opens the conversation.
-WELCOME_MSG = "Welcome to the MovieRecBot chat. Type `q` to quit. How may I serve you today?"
 
 @tool
 def get_movies() -> str:
@@ -62,66 +22,19 @@ def get_movies() -> str:
         return f"Error: {response.status_code}"
 
 @tool
-def get_movie_details(id: int) -> str:
+def get_movie_details(movie_id: int) -> str:
     """Provide the latest up-to-date movie list."""
-    response = requests.get("http://localhost:8000/movies/api/movies/"+str(id))
+    response = requests.get("http://localhost:8000/movies/api/movies/" + str(movie_id))
     if response.status_code == 200:
         return response.text
     else:
         return f"Error: {response.status_code}"
-def maybe_route_to_tools(state:AgentState) -> Literal["tools", "chatbot", "__end__"]:
-    """Routes to the tools node if the last message was a tool call."""
-    msgs = state["messages"]
-
-    # Only route based on the last message.
-    msg = msgs[-1]
-    print("ROUTING: ", msg)
-    # When the chatbot returns tool_calls, route to the "tools" node.
-    if hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
-        print("going to tools")
-        return "tools"
-    else:
-        print("going to END")
-        return END
-
-
-def chatbot(state: AgentState) -> AgentState:
-    """The chatbot itself. A wrapper around the model's own chat interface."""
-    print("IN CHATBOT----------------")
-    print("State:", state)
-    defaults = {"order": [], "finished": False}
-
-    if state["messages"] and len(state["messages"]) > 0:
-        new_output = llm_with_tools.invoke([MOVIEBOT_SYSINT] + state["messages"])
-    else:
-        # If there are no messages, start with the welcome message.
-        new_output = AIMessage(content=WELCOME_MSG)
-
-    return defaults | state | {"messages": [new_output]}
-
 
 # Define the tools and create a "tools" node.
 tools = [get_movies,get_movie_details]
-tool_node = ToolNode(tools)
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest")
-llm_with_tools = llm.bind_tools(tools)
+agent = Agent(llm, tools)
 
-# Set up the initial graph based on our state definition.
-graph_builder = StateGraph(AgentState)
-
-# Add the chatbot function to the app graph as a node called "chatbot" and the automated tools calling
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_node("tools", tool_node)
-
-
-# Decide if tools need to be called
-graph_builder.add_conditional_edges("chatbot", maybe_route_to_tools, {"tools": "tools", END: END})
-
-# Tools always route back to chat afterwards.
-graph_builder.add_edge("tools", "chatbot")
-# Define the chatbot node as the app entrypoint.
-graph_builder.add_edge(START, "chatbot")
-chat_graph = graph_builder.compile()
 
 def index(request):
     form = ChatForm()
@@ -140,7 +53,7 @@ def index(request):
             state = request.session['state']
             state.append(["user", user_input])
             print("STATE: ", state)
-            new_state = chat_graph.invoke({"messages": state})
+            new_state = agent.invoke({"messages": state})
             request.session['state'] = [[msg.type, msg.content] for msg in new_state['messages'] if msg.type != "tool" and len(msg.content)>0 ]
 
             form = ChatForm()
@@ -155,7 +68,7 @@ def index(request):
             return render(request, 'index.html', context=context)
     else:
 
-        state = chat_graph.invoke({"messages": []})
+        state = agent.invoke({"messages": []})
         request.session['state'] = [(msg.type, msg.content) for msg in state['messages']]
         context = {
             'form': form,
